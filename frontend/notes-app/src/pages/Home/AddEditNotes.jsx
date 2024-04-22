6945f0f2c6b27a6d3b1f1ea39cafe3e942733e1b
Prathik Raj RC <prathikrajrc98@gmail.com>
2024-04-22 19:25:12 +0530

    Tags

--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React , {useState}from "react";
+import TagInput from "../../components/input/TagInput";
 
 const AddEditNotes = () => {
+    
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [tags, setTags] = useState([]);
+    
     return (
         <div>
             <div className=" flex flex-col gap-2">
-                <label className="input-label">TITLE</label>
+                <label className="input-label">Title</label>
 
                 <input 
                     type="text"
                     className="text-xl text-slate-950 outline-none bg-slate-50"
                     placeholder="Go to Gym XD"
+                    value={title}
+                    onChange={({ target }) => setTitle(target.value)}
                 />
 
             </div>
@@ -20,15 +28,17 @@ const AddEditNotes = () => {
                     className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
                     placeholder="Content"
                     rows={10}
+                    value={content}
+                    onChange={({ target }) => setContent(target.value)}
                 />
             </div>
 
             <div className="mt-3">
-                <label className="input-label">TAGS</label>
+                <label className="input-label">Tags</label>
+                <TagInput tags={tags} setTags={setTags}/>
             </div>
 
             <button className="btn-primary font-medium mt-5 p-3" onClick={() =>{}}>
-
                 Add
             </button>
         </div>
