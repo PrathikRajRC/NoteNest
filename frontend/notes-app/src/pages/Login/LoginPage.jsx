7168f3476de3b713d984c309ae0be3c3eaa3ba09
Prathik Raj RC <prathikrajrc98@gmail.com>
2024-05-04 21:51:49 +0530

    login

--- frontend/notes-app/src/pages/Login/LoginPage.jsx
+++ frontend/notes-app/src/pages/Login/LoginPage.jsx
@@ -52,8 +52,8 @@ const Login = () => {
     return (
     <>
         <Navbar/>
-
-        <div className='flex items-centre justify-center mt-28'>
+        
+        <div className='flex items-centre justify-center mt-28 '>
             <div className='w-96 border rounded bg-white px-7 py-10'>
                 <form onSubmit={handleLogin}>
                     <h4 className='text-2xl mb-7'>Login</h4>
