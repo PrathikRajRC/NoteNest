2a5b990bdc556d29f9cffbb604f192266068223e
Prathik Raj RC <prathikrajrc98@gmail.com>
2024-05-04 21:16:21 +0530

    Image

--- frontend/notes-app/vite.config.js
+++ frontend/notes-app/vite.config.js
@@ -1,20 +1,26 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs';
-import path from 'path';
+// import react from '@vitejs/plugin-react'
+// import fs from 'fs';
+// import path from 'path';
 
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     https: {
+//       key: fs.readFileSync(path.resolve(__dirname, 'C:/Users/prath/OneDrive/Desktop/create-cert-key.pem')),
+//       cert: fs.readFileSync(path.resolve(__dirname, 'C:/Users/prath/OneDrive/Desktop/create-cert.pem')),
+//     },
+//     // Make sure the server is accessible over the local network
+//     host: '0.0.0.0',
+//   },
+// })
+
 export default defineConfig({
-  plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'C:/Users/prath/OneDrive/Desktop/create-cert-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'C:/Users/prath/OneDrive/Desktop/create-cert.pem')),
-    },
-    // Make sure the server is accessible over the local network
-    host: '0.0.0.0',
+    https: false, // Disable HTTPS
   },
-})
+});
 
 
